490f2666a37cd00c09b649ef8593e45d48851e1c
kamaljohnson <kamaljohnson12345@gmail.com>
2019-11-26 23:45:05 +0530

    traffic light prefab

---
+++ adaptive-traffic-signal-control-simmulation/Assets/Scripts/TrafficLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLight : MonoBehaviour
+{
+    public bool on;
+
+    public Material green;
+    public Material red;
+
+    public GameObject light;
+
+    public void ChangeLight(bool state)
+    {
+        light.GetComponent<MeshRenderer>().material = state ? green : red;
+    }
+}
